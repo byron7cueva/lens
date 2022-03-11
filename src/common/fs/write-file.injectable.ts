293513d4159dac33a00cad152e5afab40644d22a
Sebastian Malton <sebastian@malton.name>
2022-03-11 12:57:28 -0500

    Make ResourceApplier injectable
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/common/fs/write-file.injectable.ts
+++ src/common/fs/write-file.injectable.ts
@@ -6,15 +6,17 @@ import { getInjectable } from "@ogre-tools/injectable";
 import path from "path";
 import fsInjectable from "./fs.injectable";
 
+export type WriteFile = (filePath: string, data: string | Buffer) => Promise<void>;
+
 const writeFileInjectable = getInjectable({
   id: "write-file",
 
-  instantiate: (di) => {
+  instantiate: (di): WriteFile => {
     const { writeFile, ensureDir } = di.inject(fsInjectable);
 
-    return async (filePath: string, content: string | Buffer) => {
+    return async (filePath, content) => {
       await ensureDir(path.dirname(filePath), { mode: 0o755 });
-    
+
       await writeFile(filePath, content, {
         encoding: "utf-8",
       });
